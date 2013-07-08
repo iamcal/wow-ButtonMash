72e0774e9382c316927cd6bcfd9e12cc5f93cce1
Cal Henderson <cal@iamcal.com>
2013-07-08 11:31:47 -0700

    implemented shot priority. added some cooldowns on the second row

--- class_druid_moonkin.lua
+++ class_druid_moonkin.lua
@@ -2,12 +2,20 @@
 ButtonMash.Moonkin = {};
 ButtonMash.RegisterClassSpec('Moonkin', 'DRUID', 1);
 
-ButtonMash.Moonkin.SUNFIRE	= 93402;
-ButtonMash.Moonkin.MOONFIRE	= 8921;
-ButtonMash.Moonkin.WRATH	= 5176;
-ButtonMash.Moonkin.STARFIRE	= 2912;
-ButtonMash.Moonkin.STARSURGE	= 78674;
-ButtonMash.Moonkin.STARFALL	= 48505;
+ButtonMash.Moonkin.SUNFIRE		= 93402;
+ButtonMash.Moonkin.MOONFIRE		= 8921;
+ButtonMash.Moonkin.WRATH		= 5176;
+ButtonMash.Moonkin.STARFIRE		= 2912;
+ButtonMash.Moonkin.STARSURGE		= 78674;
+ButtonMash.Moonkin.STARFALL		= 48505;
+
+ButtonMash.Moonkin.FORCE_OF_NATURE	= 106737;
+ButtonMash.Moonkin.CELESTIAL_ALIGNMENT	= 112071;
+ButtonMash.Moonkin.INNERVATE		= 29166;
+ButtonMash.Moonkin.MIRROR_IMAGE		= 110621;
+
+ButtonMash.Moonkin.ECLIPSE_LUNAR	= 48518;
+ButtonMash.Moonkin.ECLIPSE_SOLAR	= 0;
 
 ButtonMash.Moonkin.SUNFIRE_NAME = GetSpellInfo(ButtonMash.Moonkin.SUNFIRE);
 
@@ -21,13 +29,17 @@ function ButtonMash.Moonkin.CreateUI()
 
 	-- main buttons
 
-	ButtonMash.CreateBoundButton('snf', 41*0, 0, 40, 40, ButtonMash.Moonkin.SUNFIRE);
-	ButtonMash.CreateBoundButton('mnf', 41*1, 0, 40, 40, ButtonMash.Moonkin.MOONFIRE);
-	ButtonMash.CreateBoundButton('wrt', 41*2, 0, 40, 40, ButtonMash.Moonkin.WRATH);
-	ButtonMash.CreateBoundButton('srf', 41*3, 0, 40, 40, ButtonMash.Moonkin.STARFIRE);
-	ButtonMash.CreateBoundButton('sts', 41*4, 0, 40, 40, ButtonMash.Moonkin.STARSURGE);
-	ButtonMash.CreateBoundButton('stf', 41*5, 0, 40, 40, ButtonMash.Moonkin.STARFALL);
+	ButtonMash.CreateBoundButton('sunfire'  , 41*0, 0, 40, 40, ButtonMash.Moonkin.SUNFIRE);
+	ButtonMash.CreateBoundButton('moonfire' , 41*1, 0, 40, 40, ButtonMash.Moonkin.MOONFIRE);
+	ButtonMash.CreateBoundButton('wrath'    , 41*2, 0, 40, 40, ButtonMash.Moonkin.WRATH);
+	ButtonMash.CreateBoundButton('starfire' , 41*3, 0, 40, 40, ButtonMash.Moonkin.STARFIRE);
+	ButtonMash.CreateBoundButton('starsurge', 41*4, 0, 40, 40, ButtonMash.Moonkin.STARSURGE);
+	ButtonMash.CreateBoundButton('starfall' , 41*5, 0, 40, 40, ButtonMash.Moonkin.STARFALL);
 
+	ButtonMash.CreateBoundButton('cd_1', 41*1, 41, 40, 40, ButtonMash.Moonkin.FORCE_OF_NATURE);
+	ButtonMash.CreateBoundButton('cd_2', 41*2, 41, 40, 40, ButtonMash.Moonkin.CELESTIAL_ALIGNMENT);
+	ButtonMash.CreateBoundButton('cd_3', 41*3, 41, 40, 40, ButtonMash.Moonkin.INNERVATE);
+	ButtonMash.CreateBoundButton('cd_4', 41*4, 41, 40, 40, ButtonMash.Moonkin.MIRROR_IMAGE);
 end
 
 function ButtonMash.Moonkin.DestroyUI()
@@ -74,5 +86,101 @@ function ButtonMash.Moonkin.UpdateFrame()
 
 
 	-- main shot state
+	local state = ButtonMash.Moonkin.GetState();
+
+	for i in pairs(ButtonMash.buttons) do
+		if (state.next == i) then
+			ButtonMash.buttons[i]:SetGlow(true);
+		else
+			ButtonMash.buttons[i]:SetGlow(false);
+		end
+	end
 
 end;
+
+function ButtonMash.Moonkin.GetState()
+
+	local out = {
+		next = '',
+	};
+
+
+	-- find out power level and any eclipses
+
+	local pow = UnitPower("player", 8);
+	local eclipse_solar = false;
+	local eclipse_lunar = false;
+
+	local i;
+	for i=1,40 do
+		local id = select(11, UnitAura("player", i));
+		if (id and id == ButtonMash.Moonkin.ECLIPSE_SOLAR) then eclipse_solar = true; end
+		if (id and id == ButtonMash.Moonkin.ECLIPSE_LUNAR) then eclipse_lunar = true; end
+	end
+
+
+	-- find out if our debuffs are on the target
+
+	local sunfire_remain = 0;
+	local moonfire_remain = 0;
+
+	for i=1,40 do
+		local id = select(11, UnitAura("target", i, "PLAYER HARMFUL"));
+
+		if (id and id == ButtonMash.Moonkin.SUNFIRE) then
+			local expire = select(7, UnitAura("target", i, "PLAYER HARMFUL"));
+			sunfire_remain = expire - GetTime();
+		end
+
+		if (id and id == ButtonMash.Moonkin.MOONFIRE) then
+			local expire = select(7, UnitAura("target", i, "PLAYER HARMFUL"));
+			moonfire_remain = expire - GetTime();
+		end
+	end
+
+
+	-- main spell priority
+
+	if (sunfire_remain < 1.5) then
+
+		out.next = 'sunfire';
+
+	elseif (moonfire_remain < 1.5) then
+
+		out.next = 'moonfire';
+
+	elseif (ButtonMash.Moonkin.TimeUntil(ButtonMash.Moonkin.STARSURGE) < 1.5) then
+
+		out.next = 'starsurge';
+
+	elseif (ButtonMash.Moonkin.TimeUntil(ButtonMash.Moonkin.STARFALL) < 1.5) then
+
+		out.next = 'starfall';
+
+	elseif (eclipse_solar) then
+
+		out.next = 'wrath';
+
+	elseif (eclipse_lunar) then
+
+		out.next = 'starfire';
+
+	elseif (pow > 0) then
+
+		out.next = 'starfire';
+
+	else
+		out.next = 'wrath';
+	end
+
+	return out;
+end;
+
+function ButtonMash.Moonkin.TimeUntil(spell)
+
+	local start, duration, enabled = GetSpellCooldown(spell);
+	if (start>0 and duration>0) then
+		return start + duration - GetTime();
+	end
+	return 0;
+end
